e4303e4af3afe29d331601488b0e2e15e7af8aa4
Hugo Estrada S. <61108175+hugoestradas@users.noreply.github.com>
2024-10-07 22:22:52 -0600

    Update check2.py

--- check2.py
+++ check2.py
@@ -2,7 +2,6 @@ from llama_index.readers.web import BeautifulSoupWebReader
 
 reader = BeautifulSoupWebReader()
 
-# Define the URL to read
 url = "https://en.wikipedia.org/wiki/Abraham_Lincoln"
 
 documents = reader.load_data(urls=[url])
